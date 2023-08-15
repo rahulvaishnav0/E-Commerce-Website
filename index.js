a880874b273d950757e120034566d217f26bb6f9
rahulvaishnav0 <141699717+rahulvaishnav0@users.noreply.github.com>
2023-08-15 11:01:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+brager =document.querySelector('.brager')
+header=document.querySelector('.navbar')
+a=document.querySelector('.a')
+brager.addEventListener('click',()=>{
+   a.classList.toggle('v-class-resp');
+   navbar.classList.toggle('h-nav-resp');
+})
